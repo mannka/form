5625c29832d9597b1480e9b0b1c8a390e29bb8d8
mannka <kmann1511@gmail.com>
2019-12-03 17:07:53 -0700

    update

--- form.js
+++ form.js
@@ -21,5 +21,5 @@ class LikeButton extends React.Component {
   }
 }
 
-const domContainer = document.querySelector("#like_button_container");
+const domContainer = document.querySelector("#customForm");
 ReactDOM.render(e(LikeButton), domContainer);
