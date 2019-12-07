791eb144e490a8d0878eda061698acfbbdfcb84b
mannka <kmann1511@gmail.com>
2019-12-07 15:08:54 -0700

    html file working

---
+++ backup_form.js
@@ -0,0 +1,40 @@
+// class Form extends React.Component {
+//   state = { name: "Hello World" };
+
+//   render() {
+//     return (
+//       <form>
+//         <fieldset style={{ border: "none" }}>
+//           <legend>Business / Organization</legend>
+//           <label>Name</label>
+//           <input
+//             type="text"
+//             name="Company"
+//             placeHolder="Legal name of Organization"
+//             maxLenth="100"
+//             required
+//           />
+//           <label>Registered Address</label>
+//           <input
+//             type="text"
+//             name="AddressLine1"
+//             placeHolder="Address Line 1"
+//             required
+//           />
+//           <input
+//             type="text"
+//             name="AddressLine1"
+//             placeHolder="Address Line 1"
+//             required
+//           />
+//         </fieldset>
+//       </form>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<Form />, document.getElementById("customForm"));
+
+window.onload = () => {
+  document.querySelector('input[name="MerchantID"]').value = "test";
+};
