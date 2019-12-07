d1542f4477cbfdb12edba0cfa51e1707dcf565cc
mannka <kmann1511@gmail.com>
2019-12-06 17:39:12 -0700

    testing Robert's code

--- form.js
+++ form.js
@@ -2,7 +2,34 @@ class Form extends React.Component {
   state = { name: "Hello World" };
 
   render() {
-    return <h1>{this.state.name}</h1>;
+    return (
+      <form>
+        <fieldset style={{ border: "none" }}>
+          <legend>Business / Organization</legend>
+          <label>Name</label>
+          <input
+            type="text"
+            name="Company"
+            placeHolder="Legal name of Organization"
+            maxLenth="100"
+            required
+          />
+          <label>Registered Address</label>
+          <input
+            type="text"
+            name="AddressLine1"
+            placeHolder="Address Line 1"
+            required
+          />
+          <input
+            type="text"
+            name="AddressLine1"
+            placeHolder="Address Line 1"
+            required
+          />
+        </fieldset>
+      </form>
+    );
   }
 }
 
