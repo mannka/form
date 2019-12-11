33739b5e7a6853665d864255b68cd70af693c2d4
mannka <kmann1511@gmail.com>
2019-12-11 11:15:29 -0700

    validation on merchant id completed

--- form.js
+++ form.js
@@ -8,12 +8,26 @@ window.onload = () => {
 
   form.addEventListener("submit", e => {
     let messages = [];
-    if (merchantIDIssuer.value === "" || merchantIDIssuer.value == null) {
-      messages.push("Merchant Issuer ID is required if Merchant ID is entered");
+    if (merchantID.value) {
+      if (merchantIDIssuer.value === "" || merchantIDIssuer.value == null) {
+        messages.push(
+          "Merchant Issuer ID is required if Merchant ID is entered"
+        );
+      }
     }
 
-    if (messages) {
+    if (merchantIDIssuer.value) {
+      if (merchantID.value === "" || merchantID.value == null) {
+        messages.push(
+          "Merchant ID is required if Merchant ID Issuer is entered"
+        );
+      }
+    }
+
+    if (messages.length > 0) {
       e.preventDefault();
+      alert("prevented");
       errorElement.innerText = messages.join(", ");
     }
   });
+};
