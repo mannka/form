5e3006cb66a12fbb996b9a9a46346327143fb031
mannka <kmann1511@gmail.com>
2019-12-27 14:25:10 -0700

    update maps

--- form.js
+++ form.js
@@ -174,4 +174,96 @@ form.addEventListener("submit", function(e) {
   }
 });
 
+var placeSearch, autocomplete;
+
+var componentForm = {
+  // street_number: "short_name",
+  // route: "long_name"
+  // locality: "long_name",
+  // administrative_area_level_1: "short_name",
+  // country: "long_name",
+  // postal_code: "short_name"
+};
+
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search predictions to
+  // geographical location types.
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("autocomplete"),
+    { types: ["geocode"] }
+  );
+
+  // Avoid paying for data that you don't need by restricting the set of
+  // place fields that are returned to just the address components.
+  autocomplete.setFields(["address_component"]);
+
+  // When the user selects an address from the drop-down, populate the
+  // address fields in the form.
+  autocomplete.addListener("place_changed", fillInAddress);
+}
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+
+  for (var component in componentForm) {
+    document.getElementById(component).value = "";
+    document.getElementById(component).disabled = false;
+  }
+
+  // Get each component of the address from the place details,
+  // and then fill-in the corresponding field on the form.
+  for (var i = 0; i < place.address_components.length; i++) {
+    var addressType = place.address_components[i].types[0];
+    if (componentForm[addressType]) {
+      var val = place.address_components[i][componentForm[addressType]];
+      document.getElementById(addressType).value = val;
+    }
+  }
+
+  document.getElementById("autocomplete").value =
+    place.address_components[0]["long_name"] +
+    " " +
+    place.address_components[1]["long_name"];
+
+  document.getElementById("city").value =
+    place.address_components[3]["long_name"];
+
+  var val;
+
+  place.address_components.forEach(e => {
+    if (e.types[0] == "administrative_area_level_1") {
+      val = e.long_name;
+    } else if (e.types[0] == "postal_code") {
+      document.getElementById("zipcode").value = e.long_name;
+    }
+  });
+  var sel = document.getElementById("state");
+  var opts = sel.options;
+  for (var opt, j = 0; (opt = opts[j]); j++) {
+    if (opt.value == val) {
+      sel.selectedIndex = j;
+      break;
+    }
+  }
+}
+
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
+      });
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
+}
+
 console.log("loaded");
